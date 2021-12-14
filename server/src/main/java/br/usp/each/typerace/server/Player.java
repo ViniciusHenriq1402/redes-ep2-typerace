12b0099ced0a13dfa39cc7b06075a19f39ea13af
swy-326 <sungwon.yoon@usp.br>
2021-12-14 11:19:17 -0300

    server

---
+++ server/src/main/java/br/usp/each/typerace/server/Player.java
@@ -0,0 +1,15 @@
+package br.usp.each.typerace.server;
+
+public class Player {
+
+	private String player;
+	private int score;
+	private 
+
+
+	public Player (String player, int score){
+
+	}
+
+
+}
--- server/src/main/java/br/usp/each/typerace/server/ServerMain.java
+++ server/src/main/java/br/usp/each/typerace/server/ServerMain.java
@@ -4,20 +4,42 @@ import org.java_websocket.server.WebSocketServer;
 
 import java.util.HashMap;
 
+
+
 public class ServerMain {
 
+
     private WebSocketServer server;
+
 
     public ServerMain(WebSocketServer server) {
         this.server = server;
     }
 
+
     public void init() {
         System.out.println("Iniciando servidor...");
-        // TODO: Implementar
+        
+        server.start();
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        
+    	int port = 8080;
+
+    	if (args.length != 0){
+
+    		try {
+    			port = Integer.parseInt(args[0]);
+    		}
+
+    		catch (NumberFormatException ex){
+    			ex.printStackTrace();
+    		}    		
+    	}
+
+
         WebSocketServer server = new Server(8080, new HashMap<>());
 
         ServerMain main = new ServerMain(server);
