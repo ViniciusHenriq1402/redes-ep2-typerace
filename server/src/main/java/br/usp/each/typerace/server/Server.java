a4aec196e7c3ed16cdd9ec2c7e7c2177e59eb27a
swy-326 <sungwon.yoon@usp.br>
2021-12-14 11:42:23 -0300

    Revert "server"
    This reverts commit 12b0099ced0a13dfa39cc7b06075a19f39ea13af.

--- server/src/main/java/br/usp/each/typerace/server/Server.java
+++ server/src/main/java/br/usp/each/typerace/server/Server.java
@@ -7,8 +7,6 @@ import org.java_websocket.server.WebSocketServer;
 import java.net.InetSocketAddress;
 import java.util.Map;
 
-// https://coderedirect.com/questions/539962/websocket-handshake-failed-in-java
-
 public class Server extends WebSocketServer {
 
     private final Map<String, WebSocket> connections;
@@ -20,36 +18,21 @@ public class Server extends WebSocketServer {
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
-
-    	connections.add(conn);
-
         // TODO: Implementar
     }
 
     @Override
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-    	
-    	connections.remove(conn);
-
         // TODO: Implementar
     }
 
     @Override
     public void onMessage(WebSocket conn, String message) {
-
-    	System.out.println("Message : " + message);
-    	conn.send(message);
-
-    	// TODO: Implementar
+        // TODO: Implementar
     }
 
     @Override
     public void onError(WebSocket conn, Exception ex) {
-
-    	ex.printStackTrace();
-
-    	connections.remove(conn);
-
         // TODO: Implementar
     }
 
--- server/src/main/java/br/usp/each/typerace/server/ServerMain.java
+++ server/src/main/java/br/usp/each/typerace/server/ServerMain.java
@@ -4,42 +4,20 @@ import org.java_websocket.server.WebSocketServer;
 
 import java.util.HashMap;
 
-
-
 public class ServerMain {
 
-
     private WebSocketServer server;
-
 
     public ServerMain(WebSocketServer server) {
         this.server = server;
     }
 
-
     public void init() {
         System.out.println("Iniciando servidor...");
-        
-        server.start();
+        // TODO: Implementar
     }
 
-
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-        
-    	int port = 8080;
-
-    	if (args.length != 0){
-
-    		try {
-    			port = Integer.parseInt(args[0]);
-    		}
-
-    		catch (NumberFormatException ex){
-    			ex.printStackTrace();
-    		}    		
-    	}
-
-
+    public static void main(String[] args) {
         WebSocketServer server = new Server(8080, new HashMap<>());
 
         ServerMain main = new ServerMain(server);
